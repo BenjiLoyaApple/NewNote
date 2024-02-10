35b6f5a7d62f7a27797f9fffca202b2cb16afd56
Benji Loya <benjiloya.apple@gmail.com>
2024-02-10 23:42:52 +0500

    Tag complete ( add view complete)

---
+++ NewNote/Note/View/EditNoteView.swift
@@ -0,0 +1,18 @@
+//
+//  EditNoteView.swift
+//  NewNote
+//
+//  Created by Benji Loya on 10.02.2024.
+//
+
+import SwiftUI
+
+struct EditNoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditNoteView()
+}
