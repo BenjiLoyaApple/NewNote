a2de95108380d4b7ae28608a2215e4a59fd961a2
Benji Loya <benjiloya.apple@gmail.com>
2024-02-10 11:18:18 +0500

    Initial Commit

---
+++ NewNote/NewNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewNoteApp.swift
+//  NewNote
+//
+//  Created by Benji Loya on 10.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
